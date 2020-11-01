406cd62fe13b353e3a5c4c2077fbd992b4ddcca9
Andrew <vogelz@gmail.com>
2020-11-01 15:27:11 -0600

    .gitignore is now working

--- Microsoft.Xrm.DevOps.Solutions.PowerShell/bin/Debug/netcoreapp3.1/debug.ps1
+++
@@ -1,2 +0,0 @@
-﻿cd C:\Git\Microsoft.Xrm.DevOps.Solutions-Github\Microsoft.Xrm.DevOps.Solutions.PowerShell\bin\Debug\netcoreapp3.1
-import-module .\Microsoft.Xrm.DevOps.Solutions.PowerShell.dll -Force
--- packages/FakeXrmEasy.9.1.57.1/tools/Install.ps1
+++
@@ -1,4 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$DTE.ItemOperations.Navigate("http://dynamicsvalue.com/get-started/nuget-install-9?version=1.57.1")
-
