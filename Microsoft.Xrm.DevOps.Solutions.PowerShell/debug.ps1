d56bcbc9bb1c4f0bc477fb40abed40fb4f70c60a
Andrew <vogelz@gmail.com>
2020-11-01 21:25:49 -0600

    Initial setup

--- Microsoft.Xrm.DevOps.Solutions.PowerShell/debug.ps1
+++ Microsoft.Xrm.DevOps.Solutions.PowerShell/debug.ps1
@@ -1,5 +1,6 @@
 ﻿cd C:\Git\Microsoft.Xrm.DevOps.Solutions-Github\Microsoft.Xrm.DevOps.Solutions.PowerShell\bin\Debug\net4.6.2
 import-module .\Microsoft.Xrm.DevOps.Solutions.PowerShell.dll -Force
+Get-CrmEarlyBoundCode -ConnectionString "sdf" -Entities "sdfdd"
 
 $ConnectionString = "authtype=ClientSecret;url=https://url.crm.dynamics.com;ClientId=clientid;ClientSecret=secret";
 #$Conn = Get-CrmConnection -ConnectionString $ConnectionString;
